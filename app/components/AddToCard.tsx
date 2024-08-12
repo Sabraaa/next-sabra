220617cecf8a222a490fddb7f9f942bfd8c9e3e8
Sabra Akhavan <100771060+Sabraaa@users.noreply.github.com>
2024-08-12 15:30:32 +0330

    made new component to use "use client;" to handlee them in client-side

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+
+const AddToCard = () => {
+  return (
+    <div>
+      <button 
+        onClick={() => {
+          "just clicked";
+        }}
+      >
+        Add to card
+      </button>
+    </div>
+  );
+};
+
+export default AddToCard;
